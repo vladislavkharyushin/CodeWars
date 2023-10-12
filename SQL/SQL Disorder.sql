6515c54cdcbcc732718590d9f7e829055f32f6e6
Vladislav <vx3110@yandex.ru>
2023-10-12 11:20:39 +0300

    Project added

---
+++ SQL/SQL Disorder.sql
@@ -0,0 +1,9 @@
+-- DESCRIPTION:
+--You are given a table numbers with just one column, number. It holds some numbers that are already ordered.
+--You need to write a query that makes them un-ordered, as in, every possible ordering should appear equally often.
+
+--SOLUTION:
+
+SELECT number
+FROM numbers
+ORDER BY RANDOM();
