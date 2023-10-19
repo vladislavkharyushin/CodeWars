0f9d0b6b8da95f1bfe82ee35701dbf7541f740bb
Vladislav <vx3110@yandex.ru>
2023-10-19 13:46:03 +0300

    Project added

---
+++ SQL/SQL Basics Maths with String Manipulations.sql
@@ -0,0 +1,14 @@
+--DESCRIPTION:
+--Given a demographics table in the following format:
+--demographics table schema
+--id
+--name
+--birthday
+--race
+--return a single column named calculation where the value is the bit 
+--length of name, added to the number of characters in race.
+
+--SOLUTION:
+
+SELECT BIT_LENGTH(name) + LENGTH(race) AS calculation
+FROM demographics;
