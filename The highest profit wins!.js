12bd73c03c475a8a2d4b4cbe2a39258089313bca
Vladislav <vx3110@yandex.ru>
2023-10-23 14:07:09 +0300

    Project added

---
+++ The highest profit wins!.js
@@ -0,0 +1,8 @@
+//DESCRIPTION:
+//Write a function that returns both the minimum and maximum number of the given list/array.
+
+//SOLUTION:
+
+function minMax(arr){
+  return [Math.min(...arr), Math.max(...arr)];
+}
