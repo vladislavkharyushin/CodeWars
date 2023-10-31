ca47c63c6ea6739773f53428fea5169ed8a9e041
Vladislav <vx3110@yandex.ru>
2023-10-31 11:59:36 +0300

    Project added

---
+++ Grasshopper - Function syntax debugging.js
@@ -0,0 +1,9 @@
+//DESCRIPTION:
+//A student was working on a function and made some syntax mistakes 
+//while coding. Help them find their mistakes and fix them.
+
+//SOLUTION:
+
+function main (verb, noun) {
+  return `${verb}${noun}`
+}
