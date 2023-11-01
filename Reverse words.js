72f67714061a97d916845055f8c66708148a8ba7
Vladislav <vx3110@yandex.ru>
2023-11-01 12:33:44 +0300

    Project added

---
+++ Reverse words.js
@@ -0,0 +1,9 @@
+//DESCRIPTION:
+//Complete the function that accepts a string parameter, and reverses each word 
+//in the string. All spaces in the string should be retained.
+
+//SOLUTION:
+
+function reverseWords(str) {
+  return str.split("").reverse().join("").split(" ").reverse().join(" ");
+}
