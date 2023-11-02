8635f5f3bba73994ba0c1cabede493f914e7d0a3
Vladislav <vx3110@yandex.ru>
2023-11-02 13:07:00 +0300

    Project added

---
+++ SQL/SQL Basics Simple FULL TEXT SEARCH.sql
@@ -0,0 +1,11 @@
+--DESCRIPTION:
+--For this challenge you need to create a simple SELECT statement. Your task is 
+--to create a query and do a FULL TEXT SEARCH. You must search the product table 
+--on the field name for the word Awesome and return each row with the given word. 
+--Your query MUST contain to_tsvector and to_tsquery PostgreSQL functions.
+
+--SOLUTION:
+
+SELECT id, name, price
+FROM product
+WHERE to_tsvector(name) @@ to_tsquery('Awesome')
