591fdd5e2bca8de61491dbf8c483e84bfc80c88a
Vladislav <vx3110@yandex.ru>
2023-11-05 18:55:55 +0300

    Project added

---
+++ Beginner - Reduce but Grow.js
@@ -0,0 +1,14 @@
+//DESCRIPTION:
+//Given a non-empty array of integers, return the result of multiplying the values together in order. 
+//Example:
+//[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+//SOLUTION:
+
+function grow(x){
+  let result = 1;
+  for (let i = 0; i < x.length; i++) {
+    result *= x[i];
+  }
+  return result;
+}
