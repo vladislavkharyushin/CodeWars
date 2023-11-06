9fcdc5e704962da55cd265f4848027f8a6c18b0b
Vladislav <vx3110@yandex.ru>
2023-11-06 13:17:02 +0300

    Project added

---
+++ Hex to Decimal.js
@@ -0,0 +1,8 @@
+//DESCRIPTION:
+//Complete the function which converts hex number (given as a string) to a decimal number.
+
+//SOLUTION:
+
+function hexToDec(hexString){
+  return  parseInt(hexString, 16);
+}
