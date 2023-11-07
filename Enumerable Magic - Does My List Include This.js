8173143e23e1ed511401f0e0386b7f8beb823483
Vladislav <vx3110@yandex.ru>
2023-11-07 06:30:02 +0300

    Project added

---
+++ Enumerable Magic - Does My List Include This.js
@@ -0,0 +1,8 @@
+//DESCRIPTION:
+//Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+//SOLUTION:
+
+function include(arr, item){
+  return arr.includes(item);
+}
