706006dd347503082e2c04dcc5b8de54a9f93aee
Vladislav <vx3110@yandex.ru>
2023-11-11 15:41:13 +0300

    Project added

---
+++ Sum The Strings.js
@@ -0,0 +1,9 @@
+//DESCRIPTION:
+//Create a function that takes 2 integers in form of a string as 
+//an input, and outputs the sum (also as a string)
+
+//SOLUTION:
+
+function sumStr(a,b) {
+  return String(Number(a) + Number(b));
+}
