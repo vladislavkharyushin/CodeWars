e762a907ca0d2fa64493dda4036d159c0054869d
Vladislav <vx3110@yandex.ru>
2023-11-14 12:11:10 +0300

    Project added

---
+++ Replace With Alphabet Position.js
@@ -0,0 +1,14 @@
+//DESCRIPTION:
+//In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+//If anything in the text isn't a letter, ignore it and don't return it.
+//"a" = 1, "b" = 2, etc.
+
+//SOLUTION:
+
+function alphabetPosition(text) {
+  return text
+    .split('')
+    .filter(i => /[a-zA-Z]/.test(i))
+    .map(j => j.toLowerCase().charCodeAt(0)-96)
+    .join(' ');
+}
