274f45c02463a39b2bc6c2458212b3bbbc898d1b
Vladislav <vx3110@yandex.ru>
2023-12-26 14:14:18 +0300

    Project added

---
+++ Catching Car Mileage Numbers.js
@@ -0,0 +1,55 @@
+//DESCRIPTION:
+//Let's make it so Bob never misses another interesting number. We've hacked into 
+//his car's computer, and we have a box hooked up that reads mileage numbers. We've 
+//got a box glued to his dash that lights up yellow or green depending on whether it 
+//receives a 1 or a 2 (respectively).
+//It's up to you, intrepid warrior, to glue the parts together. Write the function 
+//that parses the mileage number input, and returns a 2 if the number is "interesting" 
+//(see below), a 1 if an interesting number occurs within the next two miles, or a 0 
+//if the number is not interesting.
+//Note: In Haskell, we use No, Almost and Yes instead of 0, 1 and 2.
+//"Interesting" Numbers
+//Interesting numbers are 3-or-more digit numbers that meet one or more of the following criteria:
+//Any digit followed by all zeros: 100, 90000
+//Every digit is the same number: 1111
+//The digits are sequential, incementing†: 1234
+//The digits are sequential, decrementing‡: 4321
+//The digits are a palindrome: 1221 or 73837
+//The digits match one of the values in the awesomePhrases array
+//For incrementing sequences, 0 should come after 9, and not before 1, as in 7890.
+//For decrementing sequences, 0 should come after 1, and not before 9, as in 3210.
+//So, you should expect these inputs and outputs:
+// "boring" numbers
+//isInteresting(3, [1337, 256]);    0
+//isInteresting(3236, [1337, 256]); 0
+//progress as we near an "interesting" number
+//isInteresting(11207, []); 0
+//isInteresting(11208, []); 0
+//isInteresting(11209, []); 1
+//isInteresting(11210, []); 1
+//isInteresting(11211, []); 2
+//nearing a provided "awesome phrase"
+//isInteresting(1335, [1337, 256]); 1
+//isInteresting(1336, [1337, 256]); 1
+//isInteresting(1337, [1337, 256]); 2
+//Error Checking
+//A number is only interesting if it is greater than 99!
+//Input will always be an integer greater than 0, and less than 1,000,000,000.
+//The awesomePhrases array will always be provided, and will always be an array, 
+//but may be empty. (Not everyone thinks numbers spell funny words...)
+//You should only ever output 0, 1, or 2.
+
+//SOLUTION:
+
+const chars = n => n.toString().split('')
+const match = s => n => new RegExp(n).test(s)
+const tests = [
+  match('1234567890'),
+  match('9876543210'),
+  n => /^\d[0]+$/.test(n),
+  n => n == chars(n).reverse().join(''),
+  n => [...new Set(chars(n))].length === 1]
+const test = (n, xs) => n > 99 &&
+  (xs.indexOf(n) > -1 || tests.map(t => t(n)).some(x => !!x))
+const isInteresting = (n, xs) =>
+  test(n, xs) ? 2 : +(test(n + 1, xs) || test(n + 2, xs))
